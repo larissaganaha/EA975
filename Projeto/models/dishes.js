879fcac2e604a8a669c88699d8f8a0a00e5c79ad
EA975 <larissaganaha@gmail.com>
2019-06-10 17:16:23 -0300

    updating Dishes schema

--- Projeto/models/dishes.js
+++ Projeto/models/dishes.js
@@ -2,10 +2,10 @@ var mongoose = require("mongoose");
 conn3 = mongoose.createConnection('mongodb://localhost:27017/restaurantDB', {useNewUrlParser: true});
 var Schema = mongoose.Schema;
 var menuSchema = new Schema({
-    "ra": String,
-    "nome": String,
-    "curso": String,
-    "cp": Number,
-    "cr": Number
+    "name": String,
+    "price": Number,
+    "calories": Number,
+    "nutritionInfo": String,
+    "cusine": String
 });
 module.exports = conn3.model('dishes', menuSchema);
